b272d1a09eaeddb7f24b9ad08b5f603377c7ecb2
Trọng Cảnh <canhtrong26@gmail.com>
2026-08-28 14:37:51 +0700

    feat: MEX1.30 - course detail page, handle not found

---
+++ src/components/course/CourseDetail.jsx
@@ -0,0 +1,18 @@
+function CourseDetail({ course }) {
+    const { title, description, category, level, lessons, instructor, price } = course
+
+    return (
+        <div>
+            <Link to="/courses">← Quay lại danh sách</Link>
+            <span>{category}</span>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <p>{lessons} Lessons</p>
+            <p>{level}</p>
+            <p>{instructor}</p>
+            <p>${price}</p>
+        </div>
+    )
+}
+
+export default CourseDetail
--- src/pages/CourseDetailPage.jsx
+++ src/pages/CourseDetailPage.jsx
@@ -1,6 +1,21 @@
-// pages/CourseDetailPage.jsx
+import { useParams } from "react-router-dom"
+import courses from "../data/courses"
+import CourseDetail from "../components/course/CourseDetail"
+
+
 function CourseDetailPage() {
-  return <h1>Course Detail Page</h1>
+  const { id } = useParams()
+
+  const course = courses.find((item) => item.id === Number(id))
+
+  console.log("id từ URL:", id)
+  console.log("course tìm được:", course) 
+
+  if (!course) {
+    return <h2>Không tìm thấy khóa học này!</h2>
+  }
+
+  return <CourseDetail course={course} />
 }
 
 export default CourseDetailPage
